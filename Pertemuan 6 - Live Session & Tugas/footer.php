db1439a6811799ef84d2aa6523b36a1affbf6eab
Akbar Bafunazair <72426613+akbarbafunazair@users.noreply.github.com>
2020-10-26 18:34:35 +0700

    Pertemuan 6 - Live Session & Tugas

---
+++ Pertemuan 6 - Live Session & Tugas/footer.php
@@ -0,0 +1,7 @@
+<div class="container-fluid bg-dark text-white text-center p-3 n-2">
+    <div class="row">
+        <div class="col-md-12">
+         Copyright &copy Aplikasi 
+        </div>
+    </div>
+</div>
